15a04197320dd51f53ef03ef33b016c279836fc2
900Kirill009 <161521976+900Kirill009@users.noreply.github.com>
2025-12-04 04:02:43 +0300

    Add files via upload

--- DNS-Tray.ps1
+++ DNS-Tray.ps1
@@ -8,27 +8,51 @@ $Win32Helpers = Add-Type -MemberDefinition $code -Name "Win32Helpers" -PassThru
 $null = $Win32Helpers::SetProcessDPIAware()
 
 $InterfaceName = "Беспроводная сеть 2"
-$IPv4_Primary = "84.21.189.133"
-$IPv4_Secondary = "64.188.98.242"
-$IPv6_Primary = "2a12:bec4:1460:d5::2"
-$IPv6_Secondary = "2a01:ecc0:2c1:2::2"
-$DoHTemplate = "https://dns.malw.link/dns-query"
 
+# --- CONFIG MALW.LINK ---
+$Malw_IPv4_Primary = "84.21.189.133"
+$Malw_IPv4_Secondary = "64.188.98.242"
+$Malw_IPv6_Primary = "2a12:bec4:1460:d5::2"
+$Malw_IPv6_Secondary = "2a01:ecc0:2c1:2::2"
+$Malw_DoHTemplate = "https://dns.malw.link/dns-query"
+
+# --- CONFIG GEOHIDE.RU ---
+$Geo_IPv4_Primary = "95.182.120.241"
+$Geo_IPv4_Secondary = "185.87.51.182"
+$Geo_IPv6_Primary = "2a0c:9300:0:54::1"
+$Geo_IPv6_Secondary = $Malw_IPv6_Secondary 
+$Geo_DoHTemplate = "https://dns.geohide.ru:444/dns-query"
+$Geo_StatusUrl = "https://status.dns.geohide.ru/ru"
+
+# --- ICONS ---
 $iconMalw = New-Object System.Drawing.Icon ("$PSScriptRoot\okakDns.ico")
+$iconGeo  = New-Object System.Drawing.Icon ("$PSScriptRoot\ghDns.ico")
 $iconAuto = New-Object System.Drawing.Icon ("$PSScriptRoot\NoDnsCat.ico")
 
-function Set-MalwDNS {
-    Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ServerAddresses ($IPv4_Primary, $IPv4_Secondary)
-    Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ServerAddresses ($IPv6_Primary, $IPv6_Secondary)
+# --- FUNCTIONS ---
 
-    Set-DnsClientDohServerAddress -ServerAddress $IPv4_Primary -DohTemplate $DoHTemplate -AllowFallbackToUdp $false -AutoUpgrade $false
-    Set-DnsClientDohServerAddress -ServerAddress $IPv6_Primary -DohTemplate $DoHTemplate -AllowFallbackToUdp $false -AutoUpgrade $false
-    
+function Set-MalwDNS {
+    Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ServerAddresses ($Malw_IPv4_Primary, $Malw_IPv4_Secondary)
+    Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ServerAddresses ($Malw_IPv6_Primary, $Malw_IPv6_Secondary)
+    Set-DnsClientDohServerAddress -ServerAddress $Malw_IPv4_Primary -DohTemplate $Malw_DoHTemplate -AllowFallbackToUdp $false -AutoUpgrade $false
+    Set-DnsClientDohServerAddress -ServerAddress $Malw_IPv6_Primary -DohTemplate $Malw_DoHTemplate -AllowFallbackToUdp $false -AutoUpgrade $false
+    Update-MenuState
+}
+
+function Set-GeoDNS {
+    Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ServerAddresses ($Geo_IPv4_Primary, $Geo_IPv4_Secondary)
+    Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ServerAddresses ($Geo_IPv6_Primary, $Geo_IPv6_Secondary)
+    Set-DnsClientDohServerAddress -ServerAddress $Geo_IPv4_Primary -DohTemplate $Geo_DoHTemplate -AllowFallbackToUdp $false -AutoUpgrade $false
+    Set-DnsClientDohServerAddress -ServerAddress $Geo_IPv6_Primary -DohTemplate $Geo_DoHTemplate -AllowFallbackToUdp $false -AutoUpgrade $false
     Update-MenuState
 }
 
 function Reset-ToAutoDNS {
     Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ResetServerAddresses
+    Remove-DnsClientDohServerAddress -ServerAddress $Malw_IPv4_Primary -ErrorAction SilentlyContinue
+    Remove-DnsClientDohServerAddress -ServerAddress $Malw_IPv6_Primary -ErrorAction SilentlyContinue
+    Remove-DnsClientDohServerAddress -ServerAddress $Geo_IPv4_Primary -ErrorAction SilentlyContinue
+    Remove-DnsClientDohServerAddress -ServerAddress $Geo_IPv6_Primary -ErrorAction SilentlyContinue
     Update-MenuState
 }
 
@@ -37,19 +61,27 @@ function Open-NetworkSettings {
 }
 
 function Update-MenuState {
-    $ipv4Settings = Get-DnsClientServerAddress -InterfaceAlias $InterfaceName -AddressFamily IPv4
+    $ipv4Settings = Get-DnsClientServerAddress -InterfaceAlias $InterfaceName -AddressFamily IPv4 -ErrorAction SilentlyContinue
     
-    if ($ipv4Settings -and ($ipv4Settings.ServerAddresses -contains $IPv4_Primary)) {
+    $SetMalwItem.Checked = $false
+    $SetGeoItem.Checked = $false
+    $ResetItem.Checked = $false
+
+    if ($ipv4Settings -and ($ipv4Settings.ServerAddresses -contains $Malw_IPv4_Primary)) {
         $SetMalwItem.Checked = $true
-        $ResetItem.Checked = $false
         $notifyIcon.Icon = $iconMalw
     }
+    elseif ($ipv4Settings -and ($ipv4Settings.ServerAddresses -contains $Geo_IPv4_Primary)) {
+        $SetGeoItem.Checked = $true
+        $notifyIcon.Icon = $iconGeo
+    }
     else {
-        $SetMalwItem.Checked = $false
         $ResetItem.Checked = $true
         $notifyIcon.Icon = $iconAuto
     }
 }
+
+# --- UI SETUP ---
 
 $ApplicationContext = New-Object System.Windows.Forms.ApplicationContext
 
@@ -59,13 +91,23 @@ $notifyIcon.Visible = $true
 
 $contextMenu = New-Object System.Windows.Forms.ContextMenuStrip
 
+# Создаем пункты меню
 $SetMalwItem = New-Object System.Windows.Forms.ToolStripMenuItem("Установить dns.malw.link")
-$ResetItem = New-Object System.Windows.Forms.ToolStripMenuItem("Сбросить на Авто")
-$Separator = New-Object System.Windows.Forms.ToolStripSeparator
-$SettingsItem = New-Object System.Windows.Forms.ToolStripMenuItem("Открыть настройки Wi-Fi")
-$ExitItem = New-Object System.Windows.Forms.ToolStripMenuItem("Выход")
 
+$SetGeoItem  = New-Object System.Windows.Forms.ToolStripMenuItem("Установить dns.geohide.ru")
+# Добавляем пробелы в начале названия для визуального отступа (группировки)
+$GeoStatusItem = New-Object System.Windows.Forms.ToolStripMenuItem("   Статус GeoHide (с WARP)")
+
+$ResetItem   = New-Object System.Windows.Forms.ToolStripMenuItem("Сбросить на Авто")
+$Separator   = New-Object System.Windows.Forms.ToolStripSeparator
+$SettingsItem = New-Object System.Windows.Forms.ToolStripMenuItem("Открыть настройки Wi-Fi")
+$ExitItem    = New-Object System.Windows.Forms.ToolStripMenuItem("Выход")
+
+# Привязываем действия
 $SetMalwItem.add_Click({ Set-MalwDNS })
+$SetGeoItem.add_Click({ Set-GeoDNS })
+# Просто открываем ссылку
+$GeoStatusItem.add_Click({ Start-Process $Geo_StatusUrl })
 $ResetItem.add_Click({ Reset-ToAutoDNS })
 $SettingsItem.add_Click({ Open-NetworkSettings })
 $ExitItem.add_Click({
@@ -73,7 +115,10 @@ $ExitItem.add_Click({
     $ApplicationContext.ExitThread()
 })
 
-$contextMenu.Items.AddRange(@($SetMalwItem, $ResetItem, $Separator, $SettingsItem, $ExitItem))
+# Формируем порядок меню
+# GeoStatus идет сразу после установки Geo, создавая логическую группу
+$contextMenu.Items.AddRange(@($ResetItem, $SetMalwItem, $SetGeoItem, $GeoStatusItem, $Separator, $SettingsItem, $ExitItem))
+
 $notifyIcon.ContextMenuStrip = $contextMenu
 $contextMenu.add_Opening({ Update-MenuState })
 
